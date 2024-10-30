e4b0c94219d6bae27a829a483913a36fa1139442
Nicholas Zhuk <nicholaszhuk@gmail.com>
2024-10-29 23:01:15 -0700

    refactor code, added more drag-click spawn impulse

--- game.gd
+++ game.gd
@@ -15,29 +15,16 @@ var speed = 1
 
 func _input(event):
 		
+	check_spawn_sun(event)
+	check_scroll(event)
+
+		
+
+
+
+func check_scroll(event):
 	if event is not InputEventMouseButton or not can_spawn:
 		return
-		
-	if event.button_index == 1 and event.is_pressed():
-		start_vec = event.position
-	if event.button_index == 1 and event.is_released():
-		
-		var sun_instance = sun_scene.instantiate()
-		
-		var init_magnitude = start_vec.distance_to(event.position)
-		var direction = event.position.angle_to_point(start_vec)
-		
-		sun_instance.velocity.x +=  abs(init_magnitude)*cos(direction)*speed
-		sun_instance.velocity.y +=  abs(init_magnitude)*sin(direction)*speed
-		
-		
-		sun_instance.position = event.position
-		sun_instance.id = planet_counter
-		sun_instance.radius += scroll
-		planet_counter += 1
-		add_child(sun_instance)
-		
-		sun_list.append(sun_instance)
 		
 	if event.button_index == 4:
 		scroll += 1
@@ -48,11 +35,33 @@ func _input(event):
 		scroll = 0
 	elif scroll > 100:
 		scroll = 100
+
+func check_spawn_sun(event):
+	if event is not InputEventMouseButton or not can_spawn:
+		return
+		
+	if event.button_index == 1 and event.is_pressed():
+		start_vec = event.position
+	if event.button_index == 1 and event.is_released():
+		
+		var sun_instance = sun_scene.instantiate()
+		sun_instance.position = event.position
+		sun_instance.id = planet_counter
+		planet_counter += 1
+		sun_instance.radius += scroll
 		
 		
+		var init_magnitude = start_vec.distance_to(event.position)*pow(sun_instance.radius, 3)
+		var direction = event.position.angle_to_point(start_vec)
+		sun_instance.velocity.x +=  abs(init_magnitude)*cos(direction)*speed
+		sun_instance.velocity.y +=  abs(init_magnitude)*sin(direction)*speed
 		
 		
+		add_child(sun_instance)
 		
+		sun_list.append(sun_instance)
+
+
 
 func _process(delta):
 	
@@ -70,6 +79,7 @@ func _process(delta):
 			
 
 
+
 func gravity(sun, delta):
 	
 	var collided = false
@@ -78,17 +88,21 @@ func gravity(sun, delta):
 		
 		if sun.id == other_sun.id:
 			continue
+		
 		var distance = sun.position.distance_to(other_sun.position)
-		
-		
-		
 		var magnitude = ((sun.mass * other_sun.mass) / pow(distance,2))
 		var direction = sun.position.angle_to_point(other_sun.position)
 		
 		sun.velocity.x +=  abs(magnitude)*cos(direction)
 		sun.velocity.y +=  abs(magnitude)*sin(direction)
 		
-		if sun.position.distance_to(other_sun.position) <= (other_sun.radius):
+		check_collision(sun, other_sun)
+			
+	sun.position += sun.velocity*delta/sun.mass
+	
+
+func check_collision(sun, other_sun):
+	if sun.position.distance_to(other_sun.position) <= (other_sun.radius):
 			
 			#for real_other_sun in sun_list:
 				#if real_other_sun.id == other_sun.id:
@@ -98,7 +112,7 @@ func gravity(sun, delta):
 			sun.queue_free()
 			sun_list.erase(sun)
 			
-	sun.position += sun.velocity*delta/sun.mass
+
 
 
 func _on_pause_button_down():
